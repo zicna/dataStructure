194048c15fdfabb3fdedfa1c8e33fa94b5f8ec08
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-02-03 09:36:10 -0600

    add left/right_child_index methods

--- heap.rb
+++ heap.rb
@@ -10,4 +10,12 @@ class Heap
     def last_node
         @data.last
     end
+
+    def left_child_index(index)
+        return (2 * index) +1
+    end
+
+    def right_child_index(index)
+        return (2 * index) + 1
+    end
 end
