15993a889d9995b879f9286050f28bbcc4493b0f
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-02-03 09:55:32 -0600

    add insertion method with comments

--- heap.rb
+++ heap.rb
@@ -1,4 +1,6 @@
 class Heap
+
+    #! basic methods
     def initialize
         @data = []
     end
@@ -10,6 +12,8 @@ class Heap
     def last_node
         @data.last
     end
+
+    #! left/right child nodes , parent and indexes
 
     def left_child_index(index)
         return (2 * index) +1
@@ -31,5 +35,22 @@ class Heap
     def right_child(node)
         index = @data.index(node)
         return @data[index * 2 + 2]
+    end
+
+    #! insertion
+
+    def insert(value)
+    # * insert as last node
+        @data << value
+    #* keep track of the last inserted node
+        new_node_index = @data.length - 1
+    #* trickle up algorithm
+    #* condition: while new_node_index > 0 and parent node < new_node
+    while new_node_index > 0 && @data[new_node_index] < @data[parent_index(new_node_index)] 
+    # * swap child node and parent node
+        @data[new_node_index], @data[parent_index(new_node_index)] =  @data[parent_index(new_node_index)], @data[new_node_index]
+    # * update index of the new node
+        new_node_index = parent_node_index(new_node_index)
+    end
     end
 end
