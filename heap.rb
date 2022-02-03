75bc1e661dfc12262e1ed8135b856e09e506a42e
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-02-03 09:41:51 -0600

    add methods: parent_index, left_child, right_child

--- heap.rb
+++ heap.rb
@@ -16,6 +16,20 @@ class Heap
     end
 
     def right_child_index(index)
-        return (2 * index) + 1
+        return (2 * index) + 2
+    end
+
+    def parent_index(index)
+        return (index - 1) / 2
+    end
+
+    def left_child(node)
+        index = @data.index(node)
+        return @data[2 * index + 1]
+    end
+
+    def right_child(node)
+        index = @data.index(node)
+        return @data[index * 2 + 2]
     end
 end
