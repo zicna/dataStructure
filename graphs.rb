4b6836cc1634a1586e475afaec3b670965ee8656
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-02-05 23:53:44 -0600

    change 'add_adjecent_vertex' for undirected graphs

--- graphs.rb
+++ graphs.rb
@@ -6,7 +6,13 @@ class Vertex
         @adjecent_vertices = []
     end
 
-    def add_adjecent_vertex(vertex)
+    # def add_adjecent_vertex(vertex)
+    #     @adjecent_vertices << vertex
+    # end
+    # * for undirected graphs
+    def add_adjacent_vertex(vertex)
+        return if adjecent_vertices.include?(vertex)
         @adjecent_vertices << vertex
+        vertex.add_adjecent_vertex(self)
     end
 end
